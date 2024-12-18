c759f9a2ba89b979a11280f423a151bc9511cfe6
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 20:13:28 +0530

    lombok annotations are not working in Intellij Idea so following traditional setter getter and constructor methods

--- src/main/java/com/spring_boot/redis/RestAPI_Redis/models/Medicine.java
+++ src/main/java/com/spring_boot/redis/RestAPI_Redis/models/Medicine.java
@@ -1,17 +1,75 @@
 package com.spring_boot.redis.RestAPI_Redis.models;
 
-import lombok.*;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class Medicine {
+import java.io.Serializable;
+
+public class Medicine implements Serializable {
     private String id;
     private String name;
     private String expiry;
     private String companyName;
     private int count;
 
+
+    public Medicine() {
+    }
+
+    public Medicine(String id, String name, String expiry, String companyName, int count) {
+        this.id = id;
+        this.name = name;
+        this.expiry = expiry;
+        this.companyName = companyName;
+        this.count = count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Medicine{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", expiry='" + expiry + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", count=" + count +
+                '}';
+    }
 }
