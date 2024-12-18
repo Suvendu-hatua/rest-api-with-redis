e5421a99432c62927334cf4c2b496ff5327fb130
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 15:12:48 +0530

    git init

---
+++ src/main/java/com/spring_boot/redis/RestAPI_Redis/RestApiRedisApplication.java
@@ -0,0 +1,14 @@
+package com.spring_boot.redis.RestAPI_Redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRedisApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot/redis/RestAPI_Redis/RestApiRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.redis.RestAPI_Redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
